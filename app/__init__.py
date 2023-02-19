34e7ef40f39eaa866472c5fe74b9a0c4ca23ab98
dxxpxy <deep2.dhirajlal@live.uwe.ac.uk>
2023-02-19 20:28:26 +0000

    gfh

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,31 @@ app.config['SECRET_KEY'] = 'your-secret-key'
 def home():
     return render_template("home.html")
 
+@app.route('/addinvoice', methods = ['GET', 'POST'])
+def addinvoice(): 
+         
+    if request.method == 'POST':
+                
+        customername = request.form.get('customername')
+        customeraddress = request.form.get('customeraddress')
+        date = request.form.get('date')       
+        description = request.form.get('desp')
+        invoiceno = request.form.get('invoiceno')
+        invoicetotal = request.form.get('invoicetotal')
+        flash('Invoice added!', category='greenlight')
+        
+        # error = db_func.validate_invoice_form(customername, customeraddress, date, description, invoiceno, invoicetotal)
+        # if error is not None:
+        #     flash(error, category='redlight')
+        # else:
+        #     #date_obj = datetime.strptime(date, '%Y-%m-%d')  # Convert the string to a datetime object
+        #     #date_formatted = date_obj.strftime('%d/%m/%y')  # Convert the datetime object to a formatted string         
+        #     #db_func.check_db_exist()
+        #     db_func.execute_sql('INSERT INTO invoice (customername, customeraddress, date, description, invoiceno, invoicetotal) VALUES (?, ?, ?, ?, ?, ?)', customername, customeraddress, date, description, invoiceno, invoicetotal)
+        #     flash('Invoice added!', category='greenlight')
+          
+    return render_template("Form.html")
+
 
 if __name__ == '__main__':
     app.secret_key = 'asdasdasd'
