e8d44a3ece251fc0e2ecce6cb308789066779701
dxxpxy <deep2.dhirajlal@live.uwe.ac.uk>
2023-02-19 21:21:47 +0000

    sd

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, Blueprint, render_template, request, flash, redirect
 #import sqlite3
 from datetime import datetime
-from db_func import execute_sql, get_db, run_query, validate_invoice_form
+from db_func.db_func import execute_sql, get_db, run_query, validate_invoice_form
+
 #import os 
 
 app = Flask(__name__)
@@ -24,15 +25,15 @@ def addinvoice():
         invoicetotal = request.form.get('invoicetotal')
         flash('Invoice added!', category='greenlight')
         
-        # error = db_func.validate_invoice_form(customername, customeraddress, date, description, invoiceno, invoicetotal)
-        # if error is not None:
-        #     flash(error, category='redlight')
-        # else:
-        #     #date_obj = datetime.strptime(date, '%Y-%m-%d')  # Convert the string to a datetime object
-        #     #date_formatted = date_obj.strftime('%d/%m/%y')  # Convert the datetime object to a formatted string         
-        #     #db_func.check_db_exist()
-        #     db_func.execute_sql('INSERT INTO invoice (customername, customeraddress, date, description, invoiceno, invoicetotal) VALUES (?, ?, ?, ?, ?, ?)', customername, customeraddress, date, description, invoiceno, invoicetotal)
-        #     flash('Invoice added!', category='greenlight')
+        error = db_func.validate_invoice_form(customername, customeraddress, date, description, invoiceno, invoicetotal)
+        if error is not None:
+            flash(error, category='redlight')
+        else:
+            #date_obj = datetime.strptime(date, '%Y-%m-%d')  # Convert the string to a datetime object
+            #date_formatted = date_obj.strftime('%d/%m/%y')  # Convert the datetime object to a formatted string         
+            #db_func.check_db_exist()
+            db_func.execute_sql('INSERT INTO invoice (customername, customeraddress, date, description, invoiceno, invoicetotal) VALUES (?, ?, ?, ?, ?, ?)', customername, customeraddress, date, description, invoiceno, invoicetotal)
+            flash('Invoice added!', category='greenlight')
           
     return render_template("Form.html")
 
