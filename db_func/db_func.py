f1a6c41ed61c685fc45847526eeb3ccae640af18
dxxpxy <deep2.dhirajlal@live.uwe.ac.uk>
2023-02-28 15:36:07 +0000

    More comments

--- db_func/db_func.py
+++ db_func/db_func.py
@@ -2,24 +2,26 @@
 import pyodbc
 #import os 
 
+# Setting up connection string for connecting to Azure SQL database
 server = 'flaskwebapp-db-server.database.windows.net'
 database = 'FlaskWebAppDB'
 username = 'gyodicvvja@flaskwebapp-db-server.database.windows.net'
 password = 'JDVY5MHFF6B3X433$'
 driver= '{ODBC Driver 18 for SQL Server}'
-
 conn_str = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
 
-#conn = pyodbc.connect(conn_str)
-
+# This function gets a database connection
 def get_db():
     try:
+        # Try to establish a database connection using the connection string
         conn = pyodbc.connect(conn_str)
         return conn
     except pyodbc.Error as e:
+        # If an error occurs, print an error message and return None
         print(f"An error occurred while connecting to the database: {e}")
         return None
 
+# This function executes a SQL query and return results
 def run_query(sql):
     conn = get_db()
     cursor = conn.cursor()
@@ -28,7 +30,7 @@ def run_query(sql):
     conn.close()
     return results
 
-
+# This function executes a SQL statement that inserts/updates the database
 def execute_sql(sql, *args):
     conn = get_db()
     cur = conn.cursor()
@@ -36,7 +38,7 @@ def execute_sql(sql, *args):
     conn.commit()
     conn.close()
 
-
+# This function is to validate invoice form data
 def validate_invoice_form(customername, customeraddress, date, description, invoiceno, invoicetotal):
     if len(customername) < 2:
         return 'Enter customer name'
@@ -56,5 +58,5 @@ def validate_invoice_form(customername, customeraddress, date, description, invo
     if len(invoicetotal) < 1:
         return 'Enter invoice total.'
                
-    return None     
+    return None
 
